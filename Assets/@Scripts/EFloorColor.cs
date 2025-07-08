322758ecb2d81259a8e3e06ae7ba9ffc352a67af
LeeJJ07 <jkj9513@naver.com>
2025-07-08 10:33:56 +0900

    feat: add EFloorColor enum for floor color classification

---
+++ Assets/@Scripts/EFloorColor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EFloorColor
+{
+    PINK,
+    GREEN,
+    BLUE,
+    WHITE,
+    ORANGE,
+    YELLOW,
+    RED,
+    PURPLE,
+    SKYBLUE
+}
