c3439bc9d19772f3fbde0b0366d92d7e2a8e1984
LeeJJ07 <jkj9513@naver.com>
2025-07-09 00:59:13 +0900

    feat: implement GameManager singleton
    - prepares centralized control structure for future game state management

--- Assets/@Scripts/GameManager.cs
+++ Assets/@Scripts/GameManager.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null)
+                {
+                    GameObject gameManager = new GameObject("GameManager");
+                    instance = gameManager.AddComponent<GameManager>();
+                    DontDestroyOnLoad(gameManager);
+                }
+            }
+            return instance;
+        }
+    }
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Initialize();
+    }
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
-        
+        SoundManager.Instance.Initialize();
     }
 }
+
