a27df57cb62dad29884d7590414c92095f72e476
LeeJJ07 <jkj9513@naver.com>
2025-07-09 13:33:30 +0900

    refactor: remove DrawableObject and merge logic into Goods script

--- Assets/@Scripts/Goods/Goods.cs
+++ Assets/@Scripts/Goods/Goods.cs
@@ -4,7 +4,83 @@ using UnityEngine;
 
 public class Goods : MonoBehaviour
 {
+    [SerializeField] private float objectZPosition;
+    private Vector3 offsetHeightVec;
+    private Vector3 offset = new Vector3(0.165f, -0.5f, -0.285f);
+
+    private Box originalBox;
     public int ID { get { return id; } set { id = value; } }
     private int id;
 
+    private bool isDragging = false;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+
+        offsetHeightVec = Vector3.down * GetComponent<Collider>().bounds.size.y / 2.0f;
+    }
+
+    private void Update()
+    {
+        DragObject();
+    }
+
+    private void OnMouseDown()
+    {
+        isDragging = true;
+        originalBox = transform.parent.GetComponent<Box>();
+
+        GameManager.Instance.IsMotionDetect(true);
+    }
+    private void OnMouseUp()
+    {
+        isDragging = false;
+
+        GameManager.Instance.IsMotionDetect(false);
+        SoundManager.Instance.Play("MouseUpSound");
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit[] hits = Physics.RaycastAll(ray);
+
+        foreach (var hit in hits)
+        {
+            if (hit.collider.gameObject == gameObject)
+                continue;
+
+            Box targetBox = hit.collider.GetComponent<Box>();
+            if (targetBox != null && !targetBox.HasGoods)
+            {
+                if (originalBox != null)
+                    originalBox.SetGoods(0);
+
+                transform.SetParent(targetBox.transform);
+                transform.position = targetBox.transform.position + offset;
+
+                targetBox.SetGoods(GetComponent<Goods>().ID);
+                return;
+            }
+        }
+
+        transform.position = originalBox.transform.position + offset;
+    }
+
+    private void DragObject()
+    {
+        if (!isDragging)
+            return;
+
+        Vector3 targetPos = GetMouseWorldPos() + offsetHeightVec;
+        targetPos.z = objectZPosition;
+        transform.position = targetPos;
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = cam.WorldToScreenPoint(transform.position).z;
+
+        return cam.ScreenToWorldPoint(mousePoint);
+    }
 }
